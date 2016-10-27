b6ec9e2caf5d7eef5a5c5cdf51b74c2d9b01d6f0
yakovlevavaleria <yakovlevavaleri9@gmail.com>
2016-10-27 22:12:49 +0300

    Update calculator.hpp

--- include/calculator.hpp
+++ include/calculator.hpp
@@ -1,17 +1,5 @@
 double sum(double a, double b)
-{
-	return a + b;
-}
 double subtraction(double a, double b)
-{
-	return a - b;
-}
 double multiplication(double a, double b)
-{
-	return a*b;
-}
 double division(double a, double b)
-{
-	return a / b;
-}
 double power(double a, double b)
