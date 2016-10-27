84310148c2c0192f6dbd853e84c8eff7bf77213e
yakovlevavaleria <yakovlevavaleri9@gmail.com>
2016-10-27 22:10:25 +0300

    Create calculator.hpp

---
+++ include/calculator.hpp
@@ -0,0 +1,17 @@
+double sum(double a, double b)
+{
+	return a + b;
+}
+double subtraction(double a, double b)
+{
+	return a - b;
+}
+double multiplication(double a, double b)
+{
+	return a*b;
+}
+double division(double a, double b)
+{
+	return a / b;
+}
+double power(double a, double b)
