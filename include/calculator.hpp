b1a3792343ff30b6bdfa0dc4dbccc490205ed8cd
yakovlevavaleria <yakovlevavaleri9@gmail.com>
2016-11-11 15:12:04 +0400

    Update calculator.hpp

--- include/calculator.hpp
+++ include/calculator.hpp
@@ -3,3 +3,6 @@ double subtraction(double a, double b)
 double multiplication(double a, double b)
 double division(double a, double b)
 double power(double a, double b)
+double squareroot(double a);
+double absolut(double a);
+double roundp(double a);
